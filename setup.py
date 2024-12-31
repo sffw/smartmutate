c7b7434a9d0c508e61c0844a1dfa11a3399ce22f
sffw <spencerffwhite@gmail.com>
2024-12-30 23:13:11 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+setup(
+    name = 'smartMutator',
+    version = '0.0.1',
+    author = 'Spencer White',
+    author_email = '',
+    license = 'MIT',
+    description = 'Use Anthropic Claude to convert files',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = '<github url where the tool code will remain>',
+    py_modules = ['smartMutate', 'src'],
+    packages = find_packages(),
+    install_requires = [requirements],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = '''
+        [console_scripts]
+        smartMutate=smartMutate:cli
+    '''
+)
